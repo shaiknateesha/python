98b3fd62b5167b1b840d6fff824579d6514dabfa
shaiknateesha <shaiknateeshagdv2004@gmail.com>
2024-12-27 14:20:21 +0530

    Add files via upload

---
+++ swap two numbers without using third variable.py
@@ -0,0 +1,10 @@
+num1=int(input("enter the first number:"))
+num2=int(input("enter the first number:"))
+print("before swappiing:")
+print("num1=",num1)
+print("num2=",num2)
+num1,num2=num2,num1
+print("after swappiing:")
+print("num1=",num1)
+print("num2=",num2)
+
---
+++ swapping using third variable.py
@@ -0,0 +1,12 @@
+num1=int(input("enter the first number:"))
+num2=int(input("enter the second number:"))
+print("before swappiing:")
+print("num1=",num1)
+print("num2=",num2)
+temp=num1
+num1=num2
+num2=temp
+print("after swappiing:")
+print("num1=",num1)
+print("num2=",num2)
+

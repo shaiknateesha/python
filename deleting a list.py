bfe56a42f363f97ff5a6659b8ec015cec63c2cf8
shaiknateesha <shaiknateeshagdv2004@gmail.com>
2024-12-28 12:01:29 +0530

    Add files via upload

---
+++ deleting a list.py
@@ -0,0 +1,9 @@
+prog_lang=['c','c++','java','python','html','css','javascript','react js','angular','express js']
+print(prog_lang)
+print(type(prog_lang))
+print("-----------------------")
+del prog_lang[8]
+print(prog_lang)
+print("-----------------------")
+del prog_lang[0]
+print(prog_lang)

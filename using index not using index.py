963f434c253b646b9e88502c0ad47e39655a9521
shaiknateesha <shaiknateeshagdv2004@gmail.com>
2024-12-28 12:07:47 +0530

    Add files via upload

---
+++ using index not using index.py
@@ -0,0 +1,10 @@
+prog_lang=['c','c++','java','python','html','css','javascript','react js','angular','express js']
+print(prog_lang)
+print(type(prog_lang))
+print("_-------------------------------------")
+for i in prog_lang:
+    print(i)
+print("------------------------------------------")
+for i in range(len(prog_lang)):
+    print(prog_lang[i])
+    

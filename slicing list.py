5463c2aba4329dfa5938693ee70da0ba4179f54b
shaiknateesha <shaiknateeshagdv2004@gmail.com>
2024-12-28 12:14:14 +0530

    Add files via upload

---
+++ slicing list.py
@@ -0,0 +1,8 @@
+prog_lang=['c','c++','python','java','html','css','javascript','angular js','react js','express']
+print(prog_lang)
+print(type(prog_lang))
+print("---------------------")
+print("slicing of list")
+print(prog_lang[:4])
+print(prog_lang[4:])
+print(prog_lang[2:6:2])

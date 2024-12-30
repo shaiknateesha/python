2e78e7d8988e226dac4e6e06322b408986a57732
shaiknateesha <shaiknateeshagdv2004@gmail.com>
2024-12-30 11:48:46 +0530

    Add files via upload

---
+++ set method.py
@@ -0,0 +1,25 @@
+a={'hii','hello','i','mm'}
+b={'the','school','you','the'}
+print("A:",a)
+print("B:",b)
+print()
+#intersection method
+ism=a.intersection(b)
+print("intersection:",ism)
+print()
+#union method
+um=a.union(b)
+print("union:",um)
+print()
+#difference method
+dm=a.difference(b)
+print("difference:",dm)
+print()
+#issubset method
+issub=a.issubset(b)
+print("isssubset:",issub)
+print()
+#issuperset method
+issup=a.issuperset(b)
+print("issuperset:",issup)
+print()

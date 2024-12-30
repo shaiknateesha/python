7d551239ce1de7d08a21c7930602198d086479c8
shaiknateesha <shaiknateeshagdv2004@gmail.com>
2024-12-30 10:59:56 +0530

    Add files via upload

---
+++ adding a element in set.py
@@ -0,0 +1,10 @@
+set={'black','red','yellow','green','orange'}
+print(set)
+print(type(set))
+print("----------------------")
+set.add("purple")
+print(set)
+set1={'lavender','indigo','rosegold','peach'}
+set.update(set1)
+print("-------------------------------------")
+print(set)

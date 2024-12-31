b79c9fdee4d552b4f50ccdeaf8bed2d051ed1903
shaiknateesha <shaiknateeshagdv2004@gmail.com>
2024-12-31 12:33:47 +0530

    Add files via upload

---
+++ digit or not using lambda.py
@@ -0,0 +1,2 @@
+result=lambda a:print("digit") if (a>=-9 and a<=9) else print("number")
+result(45)
---
+++ two digit number using lambda.py
@@ -0,0 +1,2 @@
+result=lambda a:print("two digit") if (a>=-10 and a>=10 or a<=-10 and a<=10) else print("not two digit")
+result(9)

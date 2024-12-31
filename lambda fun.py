50d831966aea3a9989a770a18afada75aafac69a
shaiknateesha <shaiknateeshagdv2004@gmail.com>
2024-12-31 12:12:49 +0530

    Add files via upload

---
+++ lambda fun.py
@@ -0,0 +1,11 @@
+# lambada single argument
+result=lambda num:print(num)
+result(100)
+# lanbda multiple arguments 
+result=lambda a,b:print(a*b)
+result(25,6)
+#return multiple 
+add_sub=lambda x,y:(x+y,x-y) 
+a,s=add_sub(5,2)
+print(a)
+print(s)

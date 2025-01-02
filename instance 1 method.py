82de19a91ccc7a6453eba33214199bbab03dce02
shaiknateesha <shaiknateeshagdv2004@gmail.com>
2025-01-02 14:16:01 +0530

    Add files via upload

---
+++ instance 1 method.py
@@ -0,0 +1,8 @@
+class mobile:
+    def __init__(self):
+        print("object created........!")
+        self.price=15000
+    def show_price(self,price):
+        print(self.price)
+samsung=mobile()
+samsung.show_price(1000)
